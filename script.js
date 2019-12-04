2c4ddbaba142b576df4315e9cb22373124a95e4c
kingslayer2274 <farsalzalam22@gmail.com>
2019-12-04 21:27:59 +0200

    almost there

--- script.js
+++ script.js
@@ -16,3 +16,27 @@ $(window).ready(function() {
     $("#sliderTwo").addClass("sm-slider_2");
   }
 });
+
+
+
+$('body').scrollspy({ target: '#main-nav' });
+
+// Add smooth scrolling
+$('#main-nav a').on('click', function (e) {
+  // Check for a hash value
+  if (this.hash !== '') {
+    // Prevent default behavior
+    e.preventDefault();
+
+    // Store hash
+    const hash = this.hash;
+
+    // Animate smooth scroll
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function () {
+      // Add hash to URL after scroll
+      window.location.hash = hash;
+    });
+  }
+});
