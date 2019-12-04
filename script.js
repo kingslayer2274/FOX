4f1856e2f4b89f7c6ec0eb3c7d74ef7bd66e3930
kingslayer2274 <farsalzalam22@gmail.com>
2019-12-04 18:19:00 +0200

    pre last

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 $(window).ready(function() {
   var width = $(window).width();
 
-  if (width < 600) {
+  if (width > 600) {
     $(window).ready(function() {
-      $(".my_caption").addClass("resize");
+      $(".cap_slider").addClass("size-lg");
+
+      $("#sliderTwo").addClass("lg-slider_2");
     });
+  } else {
+    $(".cap_slider").removeClass("size-lg");
+    $(".cap_slider").addClass("size-sm");
+
+    $("#sliderTwo").removeClass("lg-slider_2");
+
+    $("#sliderTwo").addClass("sm-slider_2");
   }
 });
